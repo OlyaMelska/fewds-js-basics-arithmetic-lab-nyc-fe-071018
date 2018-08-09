df4094091f2d49f78677dae0b459ea1b62434327
Olha Melska <olya.melska@gmail.com>
2018-08-09 15:25:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,4 @@ let ageIsValid = Number.isInteger(currentAge);
 let randomNumber, randomInteger, randomUserID;
 randomNumber = Math.floor(Math.random()*20)+1;
 randomInteger = Math.floor(randomNumber);
-randomUserID = 
+randomUserID = randomInteger + 1000000000;
