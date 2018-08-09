02922521422ca4912ab55dcdc7dda3b9bd4c37fe
Olha Melska <olya.melska@gmail.com>
2018-08-09 15:24:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,4 @@ let newID = oldID + 1000000000;
 let ageIsValid = Number.isInteger(currentAge);
 let randomNumber, randomInteger, randomUserID;
 randomNumber = Math.floor(Math.random()*20)+1;
+randomInteger = Math.floor(randomNumber);
